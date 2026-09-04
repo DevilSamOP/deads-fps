94251112b7a0d94802e7c9c226cc221655f97c6e
DevilSamOP <samspiderop525@gmail.com>
2026-09-04 15:13:07 +0500

    AttackEntityEvent.java

--- src/main/java/me/alpha432/oyvey/event/impl/entity/player/AttackEntityEvent.java
+++ src/main/java/me/alpha432/oyvey/event/impl/entity/player/AttackEntityEvent.java
@@ -1,1 +1,16 @@
+package me.alpha432.oyvey.event.impl.entity.player;
 
+import me.alpha432.oyvey.event.Event;
+import net.minecraft.world.entity.Entity;
+
+public class AttackEntityEvent extends Event {
+    private final Entity entity;
+
+    public AttackEntityEvent(Entity entity) {
+        this.entity = entity;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+}
